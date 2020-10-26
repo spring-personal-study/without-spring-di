777fa60f53ed2ba98c78c638ab9292aa345f1f1d
chaenabi <find1086@gmail.com>
2020-10-27 02:22:20 +0900

    test beanScope(singleton, prototype)

---
+++ src/test/java/hello/core/scope/BeanScopeTest.java
@@ -0,0 +1,63 @@
+package hello.core.scope;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Scope;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+public class BeanScopeTest {
+
+    @Test
+    void singletonBeanFind() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(SingletonBean.class);
+        SingletonBean singletonBean1 = ac.getBean(SingletonBean.class);
+        SingletonBean singletonBean2 = ac.getBean(SingletonBean.class);
+        Assertions.assertThat(singletonBean1).isEqualTo(singletonBean2);
+        ac.close();
+    }
+
+    @Test
+    void prototypeBeanFind() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(ProtoTypeBean.class);
+        ProtoTypeBean protoTypeBean1 = ac.getBean(ProtoTypeBean.class);
+        ProtoTypeBean protoTypeBean2 = ac.getBean(ProtoTypeBean.class);
+        Assertions.assertThat(protoTypeBean1).isNotEqualTo(protoTypeBean2);
+        // protoTypeBean1, 2 was not call destroy !
+        // call explicitly by dev.
+        protoTypeBean1.destroy();
+        protoTypeBean2.destroy();
+
+        ac.close();
+    }
+
+    @Scope("singleton")
+    static class SingletonBean {
+
+        @PostConstruct
+        public void init() {
+            System.out.println("SingletonBean.init");
+        }
+
+        @PreDestroy
+        public void destroy() {
+            System.out.println("SingletonBean.destroy");
+        }
+    }
+
+    @Scope("prototype")
+    static class ProtoTypeBean {
+        @PostConstruct
+        public void init() {
+            System.out.println("SingletonBean.init");
+        }
+
+        @PreDestroy
+        public void destroy() {
+            System.out.println("SingletonBean.destroy");
+        }
+    }
+}
